9a27f09146c86e3a4f2afa98b53458a219094f6f
LegendaryEvenWeeb <molek.tomi07@gmail.com>
2023-08-18 23:01:50 +0200

    Major overhaul of the UI, and of the input system

--- script.py
+++ script.py
@@ -16,67 +16,80 @@ def open_score_based_window():
     print("open score based window")
     clear_main_window()
     label.config(text="Score Based Window Content")
-    
-    modeinputtext = tk.Label(text="Please input the gamemode\n(\"ground\", \"air\", \"naval\")")
-    modeinput = tk.Entry(root)
-    submodeinputtext = tk.Label(text="Input the selected sub-mode\n(\"AB\", \"RB\", \"SB\")")
-    submodeinput = tk.Entry(root)
-    rankinputtext = tk.Label(text="Please select what rank you want to play in\n(1-7)")
-    rankinput = tk.Entry(root)
-    scoreneededinputtext = tk.Label(text="How much score is required?")
+
+    mode_var = tk.StringVar(value="hidden")
+    submode_var = tk.StringVar(value="hidden")  
+    tk.Label(root, text="Select the gamemode:").grid(row=1,column=0)
+    tk.Radiobutton(root, text="Ground", variable=mode_var, value="ground").grid(row=2, column=0)
+    tk.Radiobutton(root, text="Air", variable=mode_var, value="air").grid(row=3, column=0)
+    tk.Radiobutton(root, text="Naval", variable=mode_var, value="naval").grid(row=4, column=0)
+    tk.Label(root, text="Select the sub-mode:").grid(row=5,column=0)
+    tk.Radiobutton(root, text="AB", variable=submode_var, value="AB").grid(row=6, column=0)
+    tk.Radiobutton(root, text="RB", variable=submode_var, value="RB").grid(row=7, column=0)
+    tk.Radiobutton(root, text="SB", variable=submode_var, value="SB").grid(row=8, column=0)
+    tk.Label(root, text="Please select what rank you want to play in\n(1-7)").grid(row=1,column=3)
+    rankinput = tk.Spinbox(root, from_=1, to=7)
+    tk.Label(root, text="How much score is required?").grid(row=3,column=3)
     scoreinput = tk.Entry(root)
-
-    result_label = tk.Label(root, text="", fg="green", font=font_size(16))
-
+    result_label = tk.Label(root, text="", fg="green", font=font_size(20))
 
     def validate():
         print("validation")
-        scoreinput2 = int(scoreinput.get())
-        submodeinput2 = submodeinput.get().upper()
+        scoreinput_value = scoreinput.get()
+        submodeinput2 = submode_var.get().upper()
         rankinput2 = int(rankinput.get())
-        modeinput2 = modeinput.get().lower()
+        modeinput2 = mode_var.get().lower()
+        if mode_var.get() == "hidden":
+            result_label.config(text="Select a mode", fg="red")
+            return
+        if submode_var.get() == "hidden":
+            result_label.config(text="Select a sub-mode", fg="red")
+            return
+        if not scoreinput_value:
+            result_label.config(text="Please enter the score required", fg="red")
+            return
+        try:
+            scoreinput2 = int(scoreinput_value)
+        except ValueError:
+            result_label.config(text="Invalid score value", fg="red")
+            return
         if modeinput2 not in ["ground", "air", "naval"]:
             result_label.config(text="Not valid mode", fg="red")
             return
-        if scoreinput2 < 0 or scoreinput2 > 100000:
+        if scoreinput2 > 100000:
             result_label.config(text="Too much.", fg="red")
             return
-        if ((rankinput2 <= 0 or rankinput2 >= 8) and modeinput2 != "naval") or ((rankinput2 <= 0 or rankinput2 >= 6) and modeinput2 == "naval"):
-            result_label.config(text="Outside of range", fg="red")
+        elif scoreinput2 <= 0:
+            result_label.config(text="Done.", fg="green")
+        if (rankinput2 <= 0 or rankinput2 >= 6) and modeinput2 == "naval":
+            result_label.config(text="No Such rank.", fg="red")
             return
-        if submodeinput2 not in ["AB", "RB", "SB"]:
+        if submodeinput2 not in ["AB", "RB", "SB"] or (submodeinput2 == "SB" and modeinput2 == "naval"):
             result_label.config(text="Not valid sub-mode", fg="red")
             return
-
         try:
             calculated_result = scorebased(modeinput2, submodeinput2, rankinput2, scoreinput2)
-            result_label.config(text=f"Calculated Result: {calculated_result}", fg="green", font=font_size(16))
+            result_label.config(text=f"Calculated Result: {calculated_result}", fg="green", font=font_size(20))
         except Exception as e:
-            result_label.config(text=str(e), fg="red", font=font_size(14))
+            result_label.config(text=str(e), fg="red", font=font_size(20))
 
-    confirmbutton = tk.Button(text="Calculate", command=validate)
-    confirmbutton.pack()
     def resetfields():
         print("reset fields")
-        modeinput.delete(0, tk.END)
-        submodeinput.delete(0, tk.END)
+        mode_var.set(value="hidden")
+        submode_var.set(value="hidden")
         rankinput.delete(0, tk.END)
         scoreinput.delete(0, tk.END)
         result_label.config(text="")
+        rankinput.delete(0, tk.END)
+        rankinput.insert(0, "1")
 
-    resetfield = tk.Button(text="Reset", command=resetfields)
-    modeinputtext.pack()
-    modeinput.pack()
-    submodeinputtext.pack()
-    submodeinput.pack()
-    rankinputtext.pack()
-    rankinput.pack()
-    scoreneededinputtext.pack()
-    scoreinput.pack()
-    result_label.pack()
-    resetfield.pack()
-    back_button = tk.Button(root, text="Back to Main", command=reset_main_window, width=20, height=5, font=font_size(14))
-    back_button.pack(side=tk.BOTTOM)
+    confirmbutton = tk.Button(root, text="Calculate", command=validate)
+    resetfield = tk.Button(root, text="Reset", command=resetfields)
+    confirmbutton.grid(row=5, column=3, columnspan=2, pady=10)
+    resetfield.grid(row=6, column=3)
+    scoreinput.grid(row=4,column=3)
+    rankinput.grid(row=2, column=3)
+    result_label.grid(row=10,column=0,columnspan=10)
 
 def clear_main_window():
     print("clear main window")
@@ -84,15 +97,12 @@ def clear_main_window():
         widget.pack_forget()
 
 main_window_open = False
-
 def open_main_window():
     global main_window_open
     root = tk.Tk()
-    # ... (rest of your main window setup)
     main_window_open = True
     root.mainloop()
     main_window_open = False
-
 def reset_main_window():
     global main_window_open
     if main_window_open:
